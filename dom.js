95d79a4d46b84ea9e83dcb09e7c94418e4b1fe73
pushkarsabe <pushkarsabe@gmail.com>
2023-09-15 15:29:56 +0530

    Question 7

--- dom.js
+++ dom.js
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 // // console.dir(document);
 // console.log(document.domain);
 // console.log(document.URL);
@@ -164,3 +165,64 @@ for (let i = 0; i < odd.length; i++) {
     odd[i].style.backgroundColor = 'green';
 }
 >>>>>>> d3465c2 (Question 6)
+=======
+// TRAVERSING THE DOM 
+let itemList = document.querySelector('#items');
+//parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement)
+
+// childNodes
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]); itemList.children[1].style.backgroundColor = 'yellow';
+
+// FirstChild
+// console.log(itemList.firstchild);
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// lastChild
+// console.log(itemList.lastChild);
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+
+// nextSibling
+// console.log(itemList.nextSibling);
+// nextElementSibling 
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// previousElementsibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+
+// createElement
+// Create a div
+let newDiv = document.createElement('div');
+// Add class
+newDiv.className= 'hello';
+// Add id newDiv.id = 'hello1';
+console.log(newDiv);
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+// Create text node 
+let newDivText = document.createTextNode('Hello World');
+// Add text to div
+newDiv.appendChild(newDivText);
+let container = document.querySelector('header .container'); let h1 = document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize = '30px';
+container.insertBefore(newDiv, h1);  
+>>>>>>> 120196a (Question 7)
