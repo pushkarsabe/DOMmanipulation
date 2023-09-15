087b4a41c3319ee0186fbf4fff14fec8c012b74f
pushkarsabe <pushkarsabe@gmail.com>
2023-09-15 15:09:29 +0530

    Question 4

--- dom.js
+++ dom.js
@@ -15,7 +15,11 @@
 // console.log(document.links);
 // console.log(document.images);
 
+<<<<<<< HEAD
 // //dom Manipulating 
+=======
+// //dom Manipulating
+>>>>>>> 37d6dde (Question 4)
 // document.getElementById()
 // document.getElementsByClassName()
 // document.getElementsByTagName()
@@ -36,6 +40,7 @@
 // let header = document.getElementById('main-header');
 // header.style.borderBottom = 'solid 3px #000';
 
+<<<<<<< HEAD
 // let item = document.getElementsByClassName('list-group-item');
 // console.log(item);
 
@@ -48,3 +53,35 @@ header.style.borderBottom = 'solid 3px #000';
 
 document.getElementById('headtitle').innerHTML = '<h1 id="test">Add Items</h1>'
 document.getElementById('test').style.color= 'green';
+=======
+//question 4 -  Follow the youtber uptill the 27th min
+// Write the code as per the youtuber does
+// Make the 3 rd element in the list have green background color
+// Make all the elements in the list have bold color font
+
+// let header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px #000';
+
+// document.getElementById('headtitle').innerHTML = '<h1 id="test">Add Items</h1>'
+// document.getElementById('test').style.color= 'green';
+
+// let item = document.getElementsByClassName('list-group-item');
+// console.log(item);
+// console.log(item[1]);
+// item[1].textContent = 'hello';
+// item[1].style.fontWeight = 'bold';
+// item[1].style.backgroundColor = 'yellow';
+
+// item.style.backgroundColor = 'red'; // can not apply becuase the item is array of collections
+
+// for(let i =0; i< item.length; i++){
+//     item[i].style.backgroundColor = 'red';
+//     item[i].style.color = 'white';
+// }
+
+let item = document.getElementsByClassName('list-group-item');
+item[2].style.backgroundColor = 'green';
+for(let i =0; i< item.length; i++){
+    item[i].style.fontWeight = 'bold';
+}
+>>>>>>> 37d6dde (Question 4)
