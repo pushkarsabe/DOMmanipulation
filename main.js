10e874875407e42f7df89e90c51e569dbddf437c
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-09-16 18:14:41 +0530

    Question 11

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
-//localStorage
-// localStorage.setItem('myName', 'Pushkar');
-// console.log(localStorage.getItem('myName'));
-// localStorage.removeItem('myName');
-
-// sessionStorage
-// sessionStorage.setItem('email', 'oldmail.com');
-// sessionStorage.removeItem('email');
-//values are overwritten for same keys like hashmap from java
-// sessionStorage.setItem('email', 'newmail.com');
-// console.log(sessionStorage.getItem('email'));
-
-// cookie
-// document.cookie = `name=Kyle; expires=${new Date(2025, 0, 1).toUTCString()}`;   
-// document.cookie = `lastName=Patel; expires=${new Date(9999, 0, 1).toUTCString()}`;
-// console.log(document.cookie);
+//Object serialization using json to store into local storage
 
 const myForm = document.querySelector('#my-form');
 const nameInput = document.getElementById('name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
-
+let count = 0;
 myForm.addEventListener('submit', onSubmit);
+//to remove the no of count and object after user refreshes the tabe page
+localStorage.removeItem('count');
+localStorage.removeItem('obj');
 
 function onSubmit(e) {
     e.preventDefault();
@@ -39,12 +27,55 @@ function onSubmit(e) {
         msg.classList.add('success');
         msg.innerHTML = 'Successfully loged in';
         setTimeout(() => msg.remove(), 2000);
+        // let myobj = {
+        //     name: "Pushkar",
+        //     age: 30,
+        //     job: ['student', 'intern', 'CRE', 'SD'],
+        // };
+        //this will only add just object in the storage not data
+        // console.log(myobj);
+        // localStorage.setItem('obj', myobj);
+        // console.log(localStorage.getItem('myobj'));
 
-        localStorage.setItem('name', `${nameInput.value}`);
-        localStorage.setItem('email', `${emailInput.value}`);
+        //this will add  object in the storage as well as data
+        // let myobjSerialized = JSON.stringify(myobj);
+        // localStorage.setItem('obj', myobjSerialized);
+        // console.log(localStorage.getItem('myobjSerialized'));
+        //to convert the json back to object for use
+        // let myobjDeserialized = JSON.parse(localStorage.getItem('obj'));
+        // console.log(myobjDeserialized);
+
+        //to only allow clicks on the submit button and nowhere else
+
+        let obj = {
+            name: nameInput.value,
+            email: emailInput.value
+        };
+        console.log(obj);
+        let myobjSerialized = JSON.stringify(obj);
+        localStorage.setItem('obj', myobjSerialized);
+
+        if (localStorage.getItem('count') == NaN || localStorage.getItem('count') == null) {
+            localStorage.setItem('count', 1);
+        }
+        else {
+            addCount();
+        }
+        console.log('count in localStorage= ', localStorage.getItem('count'));
 
         nameInput.value = '';
         emailInput.value = '';
     }
+
+}//onSubmit
+
+function addCount() {
+    count = parseInt(localStorage.getItem('count')) + 1;
+    // console.log('count in localStorage add funct = ', localStorage.getItem('count'));
+    localStorage.setItem('count', count);
 }
-                                     
+
+
+
+
+
