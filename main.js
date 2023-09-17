435ee91fb10c0b64703589ba7523c748f01c4bfa
Pushkar Sabe <83827883+pushkarsabe@users.noreply.github.com>
2023-09-17 11:09:52 +0530

    Question 12

--- main.js
+++ main.js
@@ -7,9 +7,8 @@ const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 let count = 0;
 myForm.addEventListener('submit', onSubmit);
-//to remove the no of count and object after user refreshes the tabe page
-localStorage.removeItem('count');
-localStorage.removeItem('obj');
+//to remove the no of count after user refreshes the page
+// localStorage.removeItem('count');
 
 function onSubmit(e) {
     e.preventDefault();
@@ -27,25 +26,6 @@ function onSubmit(e) {
         msg.classList.add('success');
         msg.innerHTML = 'Successfully loged in';
         setTimeout(() => msg.remove(), 2000);
-        // let myobj = {
-        //     name: "Pushkar",
-        //     age: 30,
-        //     job: ['student', 'intern', 'CRE', 'SD'],
-        // };
-        //this will only add just object in the storage not data
-        // console.log(myobj);
-        // localStorage.setItem('obj', myobj);
-        // console.log(localStorage.getItem('myobj'));
-
-        //this will add  object in the storage as well as data
-        // let myobjSerialized = JSON.stringify(myobj);
-        // localStorage.setItem('obj', myobjSerialized);
-        // console.log(localStorage.getItem('myobjSerialized'));
-        //to convert the json back to object for use
-        // let myobjDeserialized = JSON.parse(localStorage.getItem('obj'));
-        // console.log(myobjDeserialized);
-
-        //to only allow clicks on the submit button and nowhere else
 
         let obj = {
             name: nameInput.value,
@@ -53,15 +33,17 @@ function onSubmit(e) {
         };
         console.log(obj);
         let myobjSerialized = JSON.stringify(obj);
-        localStorage.setItem('obj', myobjSerialized);
+        //store email of the user as a key instead of object name for adding multiple 
+        // users in the local storage
+        localStorage.setItem(emailInput.value, myobjSerialized);
 
-        if (localStorage.getItem('count') == NaN || localStorage.getItem('count') == null) {
-            localStorage.setItem('count', 1);
-        }
-        else {
-            addCount();
-        }
-        console.log('count in localStorage= ', localStorage.getItem('count'));
+        // if (localStorage.getItem('count') == NaN || localStorage.getItem('count') == null) {
+        //     localStorage.setItem('count', 1);
+        // }
+        // else {
+        //     addCount();
+        // }
+        // console.log('count in localStorage= ', localStorage.getItem('count'));
 
         nameInput.value = '';
         emailInput.value = '';
@@ -69,11 +51,12 @@ function onSubmit(e) {
 
 }//onSubmit
 
-function addCount() {
-    count = parseInt(localStorage.getItem('count')) + 1;
-    // console.log('count in localStorage add funct = ', localStorage.getItem('count'));
-    localStorage.setItem('count', count);
-}
+//to count the number of users inside the local storage
+// function addCount() {
+//     count = parseInt(localStorage.getItem('count')) + 1;
+//     // console.log('count in localStorage add funct = ', localStorage.getItem('count'));
+//     localStorage.setItem('count', count);
+// }
 
 
 
